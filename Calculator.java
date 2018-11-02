ecef5abb5f55dd230acd665b72a8248715792824
MICHAEL STEFANUS WALAH <i14019@ftis>
2018-11-02 14:17:49 +0700

    Initial commit

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+class Calculator{
+	
+	public Calculator(){
+	}
+	
+	public float plus(float a, float b){
+		
+	}
+	
+	public float minus(float a, float b){
+		
+	}
+	
+	public float multiply(float a, float b){
+		
+	}
+	
+	public float division(float a, float b){
+		
+	}
+}
