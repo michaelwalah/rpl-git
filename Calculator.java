bb611beaff7dca53357cef5a6b701f1457f877ed
Mike <michael21walah.mw@gmail.com>
2018-11-05 21:30:57 +0700

    Initial Calculator Michael

--- Calculator.java
+++ Calculator.java
@@ -4,11 +4,11 @@ class Calculator{
 	}
 	
 	public float plus(float a, float b){
-		
+		return a+b;
 	}
 	
 	public float minus(float a, float b){
-		
+		return a-b;
 	}
 	
 	public float multiply(float a, float b){
